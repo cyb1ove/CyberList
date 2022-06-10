a4fbbb2978ed15de40809b9f787a4a3eb189b772
cyb1ove <cyb1ove.ua@gmail.com>
2022-06-10 13:18:01 +0300

    try to shared types

---
+++ backend/src/app/models/task.ts
@@ -0,0 +1,14 @@
+import { Task } from "../../../../common";
+import { getModelForClass, prop } from '@typegoose/typegoose';
+
+class TaskSchema {
+  @prop()
+  public text!: string;
+
+  @prop()
+  public isCompleted!: boolean;
+}
+
+const TaskModel = getModelForClass(TaskSchema);
+
+export default TaskModel;
---
+++ common/types.ts
@@ -0,0 +1,9 @@
+import { prop } from '@typegoose/typegoose';
+
+export class Task {
+  // @prop()
+  // public text!: string;
+
+  // @prop()
+  // public isCompleted!: boolean;
+}
